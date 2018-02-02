ca8f56b886c18f8720466918d11813048d966320
timo <tbueeler@gmail.com>
2018-02-02 15:53:19 +0100

    Basic Einstellungen des Programmes.

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("Test")
+#Dies ist das Projekt von Valerio und Timo
+#Projektziel:   1. Mastermind (Spiel) auf pygame installieren
+#               2. Netzwerk für Multiplayer aufbauen
+
+import pygame
+from pygame.constants import *
+
+#             R    G    B
+BLACK     = (  0,   0,   0)
+BLUE      = (  0,   0, 255)
+CYAN      = (  0, 255, 255)
+GRAY      = (100, 100, 100)
+GREEN     = (  0, 255,   0)
+ORANGE    = (255, 165,   0)
+PINK      = (255,   8, 127)
+PURPLE    = (255,   0, 255)
+RED       = (255,   0,   0)
+WHITE     = (255, 255, 255)
+YELLOW    = (255, 255,   0)
+
+
+pygame.init()
+
+# Fenstergroesse
+board_length = 900
+board_height = 700
+
+# Das Fenster erstellen
+screen = pygame.display.set_mode((board_length, board_height), 0, 32)
+pygame.display.set_caption('Mastermind')
+
+
+#Main Programm
+def main():
+    screen.fill(WHITE)
+    pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
+    print("Programm beendet.")
